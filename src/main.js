d7e99ce68b4f56cc1af2635978cb36bf1559fd36
LukaErnestini <luka.ernestini@gmail.com>
2022-05-21 12:38:11 +0100

    main setup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-// 1. Create an instance of the World app
-const world = new World(container);
+import { World } from './World/World.js';
 
-// 2. Render the scene
-world.render();
+// create the main function
+function main() {
+  // Get a reference to the container element
+  const container = document.querySelector('#scene-container');
+
+  // 1. Create an instance of the World app
+  const world = new World(container);
+
+  // 2. Render the scene
+  world.render();
+}
+
+// call main to start the app
+main();
